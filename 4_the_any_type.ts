f0a76121f4ca936de67c08201bb1338e994c8be7
jcahela <jpacahela@gmail.com>
2023-07-21 14:59:22 -0500

    initial commit, typescript notes

---
+++ 4_the_any_type.ts
@@ -0,0 +1,13 @@
+/*
+
+Variables of type 'any' can be assigned to any value and TypeScript won't give an error if they're reassigned to a different type later on.
+
+When a variable is declared without being assigned an initial value, it gets type 'any'
+
+*/
+
+let age; // Variable 'age' implicitly has an 'any' type, but a better type may be inferred from usage.
+
+age = 5; // No Error
+
+age = '5'; // No Error
