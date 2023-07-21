30034f6c405ff61805dee1ffef454562076ec6f6
jcahela <jpacahela@gmail.com>
2023-07-21 16:11:33 -0500

    add optional parameter notes

--- 8_functions.ts
+++ 8_functions.ts
@@ -6,6 +6,8 @@ TypeScript fixes this.
 
 In TypeScript, function parameters may be given type annotations, with the same syntax as variable declarations (: type)
 
+Parameters not given a type annotation are assumed to be type 'any'
+
 */
 
 function greet(name: string) {
@@ -13,9 +15,24 @@ function greet(name: string) {
 }
 
 greet(4) // Argument of type 'number' is not assignable to parameter of type 'string'.
+greet() // Expected 1 arguments, but got 0.
 
 /*
 
-Parameters not given a type annotation are assumed to be type 'any'
+Optional Parameters:
+
+TypeScript expects functions to be provided a value for all arguments
+This isn't always ideal, sometimes parameters are optional
+
+To indicate a parameter is intentionally optional in TypeScript, dd a '?' after its name.
+This tells TypeScript that the parameter is allowed to be undefined, and doesn't always have to be provided
 
 */
+
+
+function greetAnyone(name?: string) {
+  console.log(`Hello,${name || "sir/ma'am"}`)
+}
+
+
+greetAnyone(); // No Error!
