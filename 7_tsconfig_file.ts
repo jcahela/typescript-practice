b2d80ed1f98ca710a57fd60eac99f8e19a0ca9f7
jcahela <jpacahela@gmail.com>
2023-07-21 15:56:03 -0500

    Finish notes on general default values in tsconfig file

--- 7_tsconfig_file.ts
+++ 7_tsconfig_file.ts
@@ -17,3 +17,15 @@ tsconfig.json is always placed in the root of your project and you can customize
 //   },
 //   "include": ["**/*.ts"]
 // }
+
+/*
+
+  - compilerOptions - a nested object that contains t he rules for the TS compiler to enforce
+    - target - the value "es2017" means the project will use the 2017 version of EcmaScript standards of JavaScript
+    - module - this project will be using "commonjs" syntax for importing/exporting modules
+    - strictNullChecks - specifies that variables can ONLY have null or undefined values if they are EXPLICITLY ASSIGNED those values
+  - include - determines what files the compiler applies the rules to. The current value means the compiler should check every single file that has the .ts extension
+
+With a tsconfig.json file, you can use the tsc command without specifying any arguments. it will run on files specified in tsconfig.json's "include" key
+
+*/
