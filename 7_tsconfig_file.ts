7aa1e2d64901adff0406bb1a496c59cf80d5e99e
jcahela <jpacahela@gmail.com>
2023-07-21 15:51:35 -0500

    add notes on tsconfig and add tsconfig file

---
+++ 7_tsconfig_file.ts
@@ -0,0 +1,19 @@
+/*
+
+The tsconfig.json file allows us to change the default rules of TypeScript, and specifies things like telling the compiler what files to run on and more!
+
+tsconfig.json is always placed in the root of your project and you can customize what rules you want the compiler to enforce
+
+*/
+
+// ex. tsconfig.json
+
+
+// {
+//   "compilerOptions" : {
+//     "target": "es2017",
+//     "module": "commonjs",
+//     "strictNullChecks": true
+//   },
+//   "include": ["**/*.ts"]
+// }
